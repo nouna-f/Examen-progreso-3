d93549240a1116bf49a0b0f2d3c95c278f3600c3
nouna-f <nourine.founouni@udla.edu.ec>
2023-07-11 12:22:09 -0500

    En estas nuevas lineas de codigo el archivo de salida que he creado deberia abrirse y cerrarse.

--- ExamenP3.c
+++ ExamenP3.c
@@ -9,4 +9,11 @@ void corregirTexto(const char* archivoEntrada, const char* archivoSalida) {
         printf("No se pudo abrir el archivo de entrada.\n");
         return;
     }
-    
+
+     /// Abrir el archivo de salida en modo escritura
+    FILE* archivoSalidaPtr = fopen(archivoSalida, "w");
+    if (archivoSalidaPtr == NULL) {
+        printf("No se pudo abrir el archivo de salida.\n");
+        fclose(archivoEntradaPtr);
+        return;
+    }
